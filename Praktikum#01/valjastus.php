85d63f961375683c4144f4ffc54f70f62a837305
martti.lusti <martti.lusti@khk.ee>
2018-01-30 11:03:47 +0200

    Muutujate defineerimine

--- Praktikum#01/valjastus.php
+++ Praktikum#01/valjastus.php
@@ -1,10 +1,15 @@
 <?php
 /**
-  Created by PhpStorm.
-  user: martti.lusti
-  Date: 30.01.2018
-  Time: 10:24
+  * Created by PhpStorm.
+  * user: martti.lusti
+  * date: 30.01.2018
+  * time: 10:24
 */
 # Valjastamine
 echo "Aine - Veebiprogrammeerimine <br>"; # ; on kasu lõpp
 print 'Valikaine';
+# Muutujate defineerimine
+$eesNimi = 'Martti'; # eesnimi, string
+$pereNimi = 'Lusti'; # perenimi, string
+$bussiNumber = 4; # bussi number, integer
+$soiduKestvus = 0.8; # sõidu kestvus, 0.8 tundi, float
