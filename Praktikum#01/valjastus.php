e33464a6cdfe76feb03c56a3f9797b32e4a896fd
martti.lusti <martti.lusti@khk.ee>
2018-01-30 10:57:27 +0200

    Väljastamised

---
+++ Praktikum#01/valjastus.php
@@ -0,0 +1,10 @@
+<?php
+/**
+  Created by PhpStorm.
+  user: martti.lusti
+  Date: 30.01.2018
+  Time: 10:24
+*/
+# Valjastamine
+echo "Aine - Veebiprogrammeerimine <br>"; # ; on kasu lõpp
+print 'Valikaine';
