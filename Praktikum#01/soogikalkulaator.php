963f34cd2635d475c3248028aa82164a655b5c48
martti.lusti <martti.lusti@khk.ee>
2018-02-14 11:31:00 +0200

    Koostati fuktsioon soodustuse arvutamiseks

--- Praktikum#01/soogikalkulaator.php
+++ Praktikum#01/soogikalkulaator.php
@@ -10,11 +10,23 @@ $hind = 2.55;
 $soodustus = 15;
 $toetus = 1.80;
 
-#arvutamine
-if ($hind > 0 and $soodustus > 0 and $toetus > 0) {
-    $soodusHind = $hind * ((100 - $soodustus) / 100);
-    $hindopilasele = $soodusHind - $toetus;
-    echo 'Soogi hind opilasele = '.$hindopilasele;
-} else {
-    echo 'Andmed ei ole täielikud';
-}
+#funktsioon soodustuse arvutamiseks
+/**
+ * @param $taisHind
+ * @param $soodusKaart
+ * @param $kasOledOpilane
+ */
+function soogiHind($taisHind, $soodusKaart, $kasOledOpilane){
+    // funktsiooni sisu
+    $soodusHind = $taisHind;
+    $soodustusProtsent = 15; // %
+    $opilaseToetus = 1.80; // EUR
+    if($soodusKaart){
+        $soodusHind = $taisHind * ((100 - $soodustusProtsent) / 100);
+    }
+    if($kasOledOpilane){
+        $soodusHind = $soodusHind - $opilaseToetus;
+        // $soodusHind -= $opilaseToetus; // - operaatori lühendatud kuju
+    }
+    return $soodusHind;
+}// funktsiooni lõpp
