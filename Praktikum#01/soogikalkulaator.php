2f0e2c43796211a7500a128ff114495bebc1b290
martti.lusti <martti.lusti@khk.ee>
2018-02-14 11:41:54 +0200

    Kustutati väärtused massiivist

--- Praktikum#01/soogikalkulaator.php
+++ Praktikum#01/soogikalkulaator.php
@@ -11,11 +11,11 @@ $soodustus = 15;
 $toetus = 1.80;
 
 #funktsioon soodustuse arvutamiseks
-/**
- * @param $taisHind
- * @param $soodusKaart
- * @param $kasOledOpilane
- */
+//**
+* @param $taisHind
+* @param $soodusKaart
+* @param $kasOledOpilane
+*/
 function soogiHind($taisHind, $soodusKaart = false, $kasOledOpilane = false){
     // funktsiooni sisu
     $soodusHind = $taisHind;
@@ -34,16 +34,20 @@ function soogiHind($taisHind, $soodusKaart = false, $kasOledOpilane = false){
 // testimiseks paneme erinevad väärtused paika
 // kasutame selleks massiivi kujul (soodusKaart, kasOledOpilane)
 //
-$opilane = array(true, true); // olemas soodus - $opilane[0] ja oled õpilane - $opilane[1]
-$opetaja = array(true, false); // olemas soodus aga õpilane ei ole
-$kulaline = array(false, false); // pole soodustust ja pole õpilane
-// kutsume funktsiooni tööle
-// kui oled õpilane
-$soogiHind = soogiHind(2.65, $opilane[0], $opilane[1]);
-echo 'Prae hind õpilasele = '.round($soogiHind, 2).' €<br />';
-// kui olemas klinedi kaart, aga pole õpilane
-$soogiHind = soogiHind(2.65, $opetaja[0], $opetaja[1]);
-echo 'Prae hind sooduskaardi omanikule = '.round($soogiHind, 2).' €<br />';
-// kui ei ole kliendi kaarti
-$soogiHind = soogiHind(2.65, $kulaline[0], $kulaline[1]);
-echo 'Prae hind = '.round($soogiHind, 2).' €<br />';
+// kasutajad on tabel (massiiv), kus
+// 1. real on õpilase andmed
+// 2. real on õpetaja andmed
+// 3. real on külalise andmed
+$kasutajad = array(
+    array(true, true),
+    array(true, false),
+    array(false, false)
+);
+// vaatame $kasutajad masiivi läbi
+// for( tjm defineerimine; tjm kehtivuse kontroll; tjm suurendamine/vähendamine)
+for($i = 0; $i < count($kasutajad); $i++)
+{
+    // kutsume funktsiooni tööle
+    $soogiHind = soogiHind(2.65, $kasutajad[$i][0], $kasutajad[$i][1]);
+    echo 'Prae hind = '.round($soogiHind, 2).' €<br />';
+}
