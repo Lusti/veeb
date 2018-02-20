dd1aaed0be96a6bf2d4b8f4c8377ebf668ec015b
Lusti <martti.lusti@khk.ee>
2018-02-20 21:49:22 +0200

    Lisatud Proovide arv, kasutades sessioone

--- Praktikum#01/mang.php
+++ Praktikum#01/mang.php
@@ -1,30 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: martti.lusti
- * Date: 14.02.2018
- * Time: 12:31
- */
-require_once 'funktsioonid.php';
+session_start();
+
+if(!isset($_SESSION['counter'])) {
+$_SESSION['counter'] = 0;
+}
+
+if(isset($_GET['button'])) {
+++$_SESSION['counter'];
+}
+if(isset($_GET['reset'])) {
+    $_SESSION['counter'] = 0;
+}
+?>
+
+<form method="GET">
+    Sisesta number 1-50:<br>
+    <input type="hidden"    name="counter"  value="<?php echo $_SESSION['counter']; ?>" />
+    <input type="number"    name="number"   min="1" max="50">
+    <input type="submit"    name="button"   value="Vali!" />
+</form>
+<?php
 $number = 10;
 $pakutudnumber = $_GET["number"];
-loeVormFailist('mang.html');
 if  (empty($pakutudnumber)) {
     echo "";
 }   elseif ($pakutudnumber >= $number - 5 and $pakutudnumber < $number) {
-        echo "Sinu arv on natukene väiksem";
+    echo "Sinu arv on natukene väiksem";
 }   elseif ($pakutudnumber <= $number + 5 and $pakutudnumber > $number) {
-        echo "Sinu arv on natukene Suurem";
+    echo "Sinu arv on natukene Suurem";
 }   elseif ($pakutudnumber< "$number") {
-        echo "Liiga väike arv";
+    echo "Liiga väike arv";
 }   elseif ($pakutudnumber> "$number") {
-        echo "Liiga suur arv";
+    echo "Liiga suur arv";
 }   elseif ($pakutudnumber = $number) {
-        echo "Palju Õnne!";
+    echo "Palju Õnne!";
 }
-$counter = 0;
-while(isset($_GET['submit'])) {
-        $counter++;
-        break;
-}
-echo "Proov:" .$counter;
+
+echo "<br>Proov:" .$_SESSION['counter'];
+?>
+<form mehot="GET">
+    <br>
+    <input type="submit" name="reset" Value="Alusta Uuesti!">
+</form>
