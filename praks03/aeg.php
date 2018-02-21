47d27010eb8675862f52f2471b232155e072948a
martti.lusti <martti.lusti@khk.ee>
2018-02-21 11:12:08 +0200

    Andmebaasi lisamiseks read lisatud ( hetkel kommenteeritud, et ei segaks)

--- praks03/aeg.php
+++ praks03/aeg.php
@@ -55,3 +55,27 @@ $aeg = $_POST["kuu"].".".$_POST["paev"].".".$_POST["aasta"];
 #echo $aeg.' ,   '.$eesnimi.'    ,   '.$perenimi;
 echo "Sisestatud nimi:".$eesnimi." ".$perenimi."<br>";
 echo "Sisestatud kuupäev:".$aeg;
+# Andmebaasi saatmiseks
+$servername = "localhost";
+$username = "Saladus";
+$password = "Saladus";
+$dbname = "Pole tehtud";
+
+// Create connection
+#$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+#if ($conn->connect_error) {
+#    die("Connection failed: " . $conn->connect_error);
+#}
+#
+#$sql = "INSERT INTO nimed (eesnimi, perenimi, aeg)
+#VALUES ('$eesnimi', '$perenimi', '$aeg')";
+#
+#if ($conn->query($sql) === TRUE) {
+#    echo "New record created successfully";
+#} else {
+#    echo "Error: " . $sql . "<br>" . $conn->error;
+#}
+#
+#$conn->close();
+echo "<br><br>INSERT INTO nimed (eesnimi, perenimi, aeg) VALUES ('$eesnimi', '$perenimi', '$aeg')";
