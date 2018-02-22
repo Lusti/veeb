b44140a3206fb879809d6ccbe314f022d6a7dfb8
martti.lusti <martti.lusti@khk.ee>
2018-02-22 13:25:53 +0200

    Uncommentitud andmebaasi lisamis read.

--- praks03/aeg.php
+++ praks03/aeg.php
@@ -23,31 +23,31 @@ loeVormFailist('aegvorm.html');
 
 $eesnimi = $_POST["eesnimi"];
 $perenimi = $_POST["perenimi"];
-$aeg = $_POST["paev"].".".$_POST["kuu"].".".$_POST["aasta"];
+$aeg = $_POST["aasta"]."-".$_POST["kuu"]."-".$_POST["paev"];
 #echo $aeg.' ,   '.$eesnimi.'    ,   '.$perenimi;
 echo "Sisestatud nimi:".$eesnimi." ".$perenimi."<br>";
 echo "Sisestatud kuupäev:".$aeg;
 # Andmebaasi saatmiseks
 $servername = "localhost";
-$username = "Saladus";
-$password = "Saladus";
-$dbname = "Pole tehtud";
+$username = "lustimarttiiktkh";
+$password = "Tyra12345";
+$dbname = "lustimar_test";
 
 // Create connection
-#$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-#if ($conn->connect_error) {
-#    die("Connection failed: " . $conn->connect_error);
-#}
-#
-#$sql = "INSERT INTO nimed (eesnimi, perenimi, aeg)
-#VALUES ('$eesnimi', '$perenimi', '$aeg')";
-#
-#if ($conn->query($sql) === TRUE) {
-#    echo "New record created successfully";
-#} else {
-#    echo "Error: " . $sql . "<br>" . $conn->error;
-#}
-#
-#$conn->close();
-echo "<br><br>INSERT INTO nimed (eesnimi, perenimi, aeg) VALUES ('$eesnimi', '$perenimi', '$aeg')";
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO andmed (eesnimi, perenimi, aeg)
+VALUES ('$eesnimi', '$perenimi', '$aeg')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+echo "<br><br>INSERT INTO andmed (eesnimi, perenimi, aeg) VALUES ('$eesnimi', '$perenimi', '$aeg')";
